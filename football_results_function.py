44c67c756fdba766ed9ec353d5497a6736ad6bf1
Anson Ming <128398768+AnsonMing@users.noreply.github.com>
2023-05-10 23:15:32 +0800

    Update football_results_function.py

--- football_results_function.py
+++ football_results_function.py
@@ -2,26 +2,23 @@ import json
 import urllib.request
 import time
 import datetime
-import ast
-import csv
 from selenium import webdriver
+import mysql.connector
 
 
 class http_req_results():
-    def __init__(self, date='2016/01/01', Num_day=1, file_address='football_results.py'):
+    def __init__(self, date='2016/01/01', Num_day=1):
         self.date_format = '%Y/%m/%d'
         self.date = datetime.datetime.strptime(date, self.date_format)
+        self.nextdate = self.date+datetime.timedelta(days=1)
         self.start_date = str(
             self.date+datetime.timedelta(days=0)).replace('-', '')[:8]
         self.end_date = str(
             self.date+datetime.timedelta(days=Num_day)).replace('-', '')[:8]
         self.mark_date = f'{self.end_date[:4]}/{self.end_date[4:6]}/{self.end_date[6:8]}'
         self.today_date = str(datetime.datetime.now()).replace('-', '')[:8]
-        self.file = file_address
-        self.count = 0
         self.data_format = {
             'officialID': '',
-            'MatchID': '',
             'date': '',
             'homeTeam_CH': '',  # 主隊中文名
             'homeTeam_EN': '',  # 主隊英文名
@@ -29,14 +26,10 @@ class http_req_results():
             'awayTeam_EN': '',  # 客隊英文名
             'tShortCH': '',  # 聯賽中文名
             'tShortEN': '',  # 聯賽英文名
-            'crs': '',  # 全場波膽
-            'fcs': '',  # 半場波膽
-            'fha': '',  # 半埸主客和
+            'half_score': '',  # 上半比分
+            'full_score': '',  # 全埸比分
             'fts': '',  # 首隊入球
-            'had': '',  # 全場主客和
-            'hft': '',  # 半全埸
-            'ooe': '',  # 入球單雙
-            'ttg': ''  # 總入球
+            'chl': ''  # 角球
         }
         self.url_findSourse = f'https://bet.hkjc.com/football/getJSON.aspx?jsontype=search_result.aspx&startdate={self.start_date}&enddate={self.end_date}&teamid=default'
         self.header = {'cookie': '',
@@ -44,161 +37,158 @@ class http_req_results():
         #print(self.start_date, self.end_date, self.mark_date, self.today_date)
 
     def get_cookie(self):
-        browser = webdriver.Chrome()
-        browser.get('https://bet.hkjc.com/football/')
-        time.sleep(10)
-        dictcookie = browser.get_cookies()
         cookies_results = ''
-        for i in dictcookie:
-            cookies_results += f"{i['name']}={i['value']}; "
-        # print(cookies_results)
+        try:
+            browser = webdriver.Chrome()
+            browser.get('https://bet.hkjc.com/football/')
+            time.sleep(5)
+            dictcookie = browser.get_cookies()
+            for i in dictcookie:
+                cookies_results += f"{i['name']}={i['value']}; "
+            print(cookies_results)
+        except Exception as Error:
+            print(Error)
         return cookies_results
 
-    def matches_count(self, url, header):
-        #print(url, header)
-        print(f'start | {self.start_date} - {self.end_date}')
-        match_count = 0
-        for i in range(20):
-            try:
-                time.sleep(5)
-                with urllib.request.urlopen(urllib.request.Request(url, headers=header)) as response:
-                    data_txt = response.read().decode('utf-8')
-                results = json.loads(data_txt)
-                match_count = results[0]['matchescount']
-                print(f'count | {match_count}')
-                return str(match_count)
-            except Exception as Error:
-                print(f'matches_count | {Error}')
-        return False
-
-    def show_date(self):
-        print(f"start_date {self.start_date} - end_date {self.end_date}")
-
-    def matches_url(self):
-        page_no = 2  # &pageno=2
-        self.header['cookie'] = self.get_cookie()
-        match_count=self.matches_count(self.url_findSourse, self.header)
-        if match_count:
-            count = int(match_count)
-            gen_url = []
-            gen_url.append(self.url_findSourse)
-            count -= 20
-            while True:
-                try:
-                    if count >= 20:
-                        tmp_url = (self.url_findSourse+'&pageno='+str(page_no))
-                        page_no += 1
-                        count -= 20
-                        gen_url.append(str(tmp_url))
-                    elif count > 0:
-                        tmp_url = (self.url_findSourse+'&pageno='+str(page_no))
-                        gen_url.append(str(tmp_url))
-                        return gen_url, count
-                    else:
-                        return gen_url, count
-                except Exception as Error:
-                    print(f'def matches_url | {Error}')
-        else:
-            return False
-
-    def get_results(self):
-        tmp_list = []
-        url_link, count = self.matches_url()
-        #print(url_link , count)
-        for url in url_link:
+    def get_resutls(self):
+        page_no = 1
+        results_list = []
+        while True:
+            print(f'page_no | {page_no}')
+            url = (self.url_findSourse+'&pageno='+str(page_no))
             with urllib.request.urlopen(urllib.request.Request(url, headers=self.header)) as response:
                 data_txt = response.read().decode('utf-8')
-            results = json.loads(data_txt)
-            page = results[0]['matches']
-            while True:
-                if url == url_link[-1]:
-                    mark = count
-                else:
-                    mark = 20
-                for i in range(mark):
-                    tmp_results_dict = self.data_format
-                    for tmp_results, source in [('officialID', page[i]['matchIDinofficial']),
-                                                ('officialID',
-                                                page[i]['matchIDinofficial']),
-                                                ('MatchID',
-                                                 page[i]['matchID']),
-                                                ('date',
-                                                page[i]['matchIDinofficial'][:8]),
-                                                ('homeTeam_CH',
-                                                page[i]['homeTeam']['teamNameCH']),
-                                                ('homeTeam_EN',
-                                                page[i]['homeTeam']['teamNameEN']),
-                                                ('awayTeam_CH',
-                                                page[i]['awayTeam']['teamNameCH']),
-                                                ('awayTeam_EN',
-                                                page[i]['awayTeam']['teamNameEN']),
-                                                ('tShortCH',
-                                                page[i]['tournament']['tShortCH']),
-                                                ('tShortEN',
-                                                page[i]['tournament']['tShortEN']),
-                                                ('crs', page[i]['results']
-                                                ['CRS'].replace(':', '')),
-                                                ('fcs', page[i]['results']
-                                                ['FCS'].replace(':', '')),
-                                                ('fha', page[i]
-                                                 ['results']['FHA']),
-                                                ('fts', page[i]
-                                                 ['results']['FTS']),
-                                                ('had', page[i]
-                                                 ['results']['HAD']),
-                                                ('hft', page[i]['results']
-                                                ['HFT'].replace(':', '')),
-                                                ('ooe', page[i]
-                                                 ['results']['OOE']),
-                                                ('ttg', page[i]['results']['TTG'])]:
-                        try:
-                            tmp_results_dict[tmp_results] = source
-                        except Exception as Error:
-                            print(Error)
-                            pass
-                    with open(self.file, 'a', encoding='utf-8') as txt:  # save data
-                        txt.write(f'{str(tmp_results_dict)}\n')
-                    tmp_list.append(i)
-                if len(tmp_list) == 20:  # check finish
-                    tmp_list = []
-                    break
-                elif 20 > len(tmp_list) > 0:  # check count
-                    if len(tmp_list) == mark:
-                        break
-        list = []
-        with open(self.file, 'r', encoding='utf-8') as txt:
-            for i in txt.readlines():
-                list.append(ast.literal_eval(i.replace('\n', '')))
-        return list
+            try:
+                results = json.loads(data_txt)
+            except Exception as Error:
+                print(Error)
+                self.header['cookie'] = self.get_cookie()
+                continue
+            if len(results):
+                results_ = results[0]['matches']
+            else:
+                return 0
+            for i in results_:
+                results_list.append(i)
+            if (int(results[0]['matchescount'])/page_no) == 20:
+                print(f'get | {len(results_list)}')
+                return results_list
+            elif len(results_) != 20:
+                print(f'get | {len(results_list)}')
+                return results_list
+            page_no += 1
+    # input data
 
-    def gen_csv(self, csv_name):
-        # Open CSV file in write mode
-        with open(csv_name, 'w', newline='',encoding='utf-8') as csvfile:
-            # Create CSV writer object
-            writer = csv.writer(csvfile)
-            writer.writerow(['officialID',
-            'MatchID',
-            'date',
-            'homeTeam_CH',
-            'homeTeam_EN',
-            'awayTeam_CH',
-            'awayTeam_EN',
-            'tShortCH',
-            'tShortEN',
-            'crs',
-            'fcs',
-            'fha',
-            'fts',
-            'had',
-            'hft',
-            'ooe',
-            'ttg'])
-            # Write data to CSV file
-            for i in self.get_results():
-                writer.writerow([i['officialID'],i['MatchID'],i['date'],i['homeTeam_CH'],i['homeTeam_EN'],i['awayTeam_CH'],i['awayTeam_EN'],i['tShortCH'],i['tShortEN'],i['crs'],i['fcs'],i['fha'],i['fts'],i['had'],i['hft'],i['ooe'],i['ttg']])
+    def input_data(self, target, source):
+        if 'matchIDinofficial' in source:
+            target['officialID'] = source['matchIDinofficial']
+            target['yyyy'] = int(source['matchIDinofficial'][:4])
+            target['mm'] = int(source['matchIDinofficial'][4:6])
+            target['dd'] = int(source['matchIDinofficial'][6:8])
+        if 'homeTeam' in source:
+            target['homeTeam_CH'] = source['homeTeam']['teamNameCH']
+            target['homeTeam_EN'] = source['homeTeam']['teamNameEN']
+        if 'awayTeam' in source:
+            target['awayTeam_CH'] = source['awayTeam']['teamNameCH']
+            target['awayTeam_EN'] = source['awayTeam']['teamNameEN']
+        if 'tournament' in source:
+            target['tShortCH'] = source['tournament']['tShortCH']
+            target['tShortEN'] = source['tournament']['tShortEN']
+        if 'cornerresult' in source:
+            target['chl'] = source['cornerresult']
+        else:
+            target['chl'] = ''
+        if 'accumulatedscore' in source:
+            if len(source['accumulatedscore']) == 2:
+                target['half_score'] = str(
+                    source['accumulatedscore'][0][
+
+                        'home'])+'|'+str(source['accumulatedscore'][0]['away'])
+                target['full_score'] = str(
+                    source['accumulatedscore'][1]['home'])+'|'+str(source['accumulatedscore'][0]['away'])
+            else:
+                target['half_score'] = ''
+                target['full_score'] = ''
+        else:
+            target['half_score'] = ''
+            target['full_score'] = ''
+        if 'results' in source:
+            if 'FTS' in source['results']:
+                target['fts'] = source['results']['FTS']
+            else:
+                target['fts'] = ''
+        else:
+            target['fts'] = ''
+
+    def data_Filter(self, gen_file=False, file_name='results'):
+        data_list = []
+        count = 0
+        results_list = self.get_resutls()
+        if results_list:
+            for i in results_list:
+                data_list.append({})
+                self.input_data(target=data_list[count], source=i)
+                count += 1
+            return data_list
+        else:
+            return 0
+
+    # set & execute sql
+    def set_db(self, host, port, user, passwd, database):
+        self.db_connect = mysql.connector.connect(
+            host=host, port=port, user=user, passwd=passwd, database=database)
+        self.cursor = self.db_connect.cursor()
+
+    # up to sql
+    def up_to_sql(self):
+        data_list = self.data_Filter()
+        if not(data_list):
+            return
+        for match in data_list:
+            print(match['officialID'])
+            # update data
+            self.cursor.execute(
+                """select * from `football_results` where `officialID`=%s;""", (match['officialID'],))
+            countrow = self.cursor.fetchall()
+            if len(countrow):
+                self.cursor.execute(
+                    '''update `football_results` set yyyy=%s,mm=%s,dd=%s,homeTeam_CH=%s,homeTeam_EN=%s,awayTeam_CH=%s,awayTeam_EN=%s,tShortCH=%s,tShortEN=%s,half_score=%s,full_score=%s,fts=%s,chl=%s where officialID=%s''',
+                    (match['yyyy'], match['mm'], match['dd'], match['homeTeam_CH'], match['homeTeam_EN'], match['awayTeam_CH'], match['awayTeam_EN'], match['tShortCH'],
+                     match['tShortEN'], match['half_score'], match['full_score'], match['fts'], match['chl'], match['officialID'])
+                )
+            else:
+                self.cursor.execute(
+                    '''insert into `football_results` (officialID,yyyy,mm,dd,homeTeam_CH,homeTeam_EN,awayTeam_CH,awayTeam_EN,tShortCH,tShortEN,half_score,full_score,fts,chl) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',
+                    (match['officialID'], match['yyyy'], match['mm'], match['dd'], match['homeTeam_CH'], match['homeTeam_EN'], match['awayTeam_CH'], match['awayTeam_EN'],
+                     match['tShortCH'], match['tShortEN'], match['half_score'], match['full_score'], match['fts'], match['chl'])
+                )
+            self.db_connect.commit()
+        data_list.clear()
+
+    def auto_run(self):
+        print(self.today_date)
+        while True:
+            print(self.start_date)
+            self.up_to_sql()
+            self.date = self.date+datetime.timedelta(days=1)
+            self.start_date = str(
+                self.date+datetime.timedelta(days=0)).replace('-', '')[:8]
+            self.end_date = str(
+                self.date+datetime.timedelta(days=1)).replace('-', '')[:8]
+
+            if self.start_date == self.today_date:
+                return
 
 
-
-if __name__ == "__main__":
-    http_req_results('2016/01/01', 1,
-                     'football_results').gen_csv('results.csv')
+if __name__ == '__main__':
+    date_ = {'yyyy/mm/dd'}
+    while True:
+        a = http_req_results(date=date_)
+        a.set_db(host={},
+                 port={},
+                 user={},
+                 passwd={},
+                 database={})
+        a.up_to_sql()
+        date_ = a.end_date[:4]+'/'+a.end_date[4:6]+'/'+a.end_date[6:8]
+        print(date_)
